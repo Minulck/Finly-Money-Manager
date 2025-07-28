b10e87892d68e8947187e1dd2234faefec570218
minulck <minulck@gmail.com>
2025-07-29 00:13:50 +0530

    Add FinlyBackendApplication class to initialize Spring Boot application

---
+++ finly-backend/src/main/java/com/Minul/finly_backend/FinlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Minul.finly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinlyBackendApplication.class, args);
+	}
+
+}
