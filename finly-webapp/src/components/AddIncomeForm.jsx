7d8c147036d4788e3f91592aaad8e0fdd63e3dbd
minulck <minulck@gmail.com>
2025-08-04 15:34:57 +0530

    feat: Add income management form with validation and category fetching

---
+++ finly-webapp/src/components/AddIncomeForm.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import Input from "./Input";
+import EmojiPickerPopup from "./EmojiPickerPopup";
+
+
+const AddIncomeForm = ({ onAddIncome, categories }) => {
+  const [income, setIncome] = useState({
+    name: "",
+    amount: "",
+    date: "",
+    categoryId: "",
+    icon: "",
+  });
+
+  const categoryOptions = categories.map((category) => {
+    return {
+      value: category.id,
+      label: category.name,
+    };
+  });
+
+  const handleChange = (key, value) => {
+    setIncome({ ...income, [key]: value });
+  };
+
+  return (
+    <div>
+      <EmojiPickerPopup
+        onSelect={(emoji) => handleChange("icon", emoji)}
+        icon={income.icon}
+      />
+      <Input
+        onChange={({ target }) => handleChange("name", target.value)}
+        label="Income Name"
+        placeholder="e.g Salary, Freelancing"
+        value={income.name}
+        type="text"
+      />
+      <Input
+        label="Income Category"
+        value={income.categoryId}
+        onChange={({ target }) => handleChange("categoryId", target.value)}
+        isSelect
+        option={categoryOptions}
+      />
+      <Input
+        onChange={({ target }) => handleChange("amount", target.value)}
+        label="Income Amount"
+        placeholder="e.g 5000, 200"
+        value={income.amount}
+        type="number"
+      />
+        <Input
+            onChange={({ target }) => handleChange("date", target.value)}
+            label="Income Date"
+            value={income.date}
+            type="date"
+      />
+      <div className="flex justify-end mt-6">
+        <button
+          type="button"
+          onClick={() => onAddIncome(income)}
+          className="bg-emerald-600 text-white px-4 py-2 rounded hover:bg-emerald-500 transition-colors"
+        >
+          Save Income
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddIncomeForm;
--- finly-webapp/src/pages/Income.jsx
+++ finly-webapp/src/pages/Income.jsx
@@ -4,6 +4,10 @@ import { useUser } from "../hooks/useUserHook";
 import { API_ENDPOINTS } from "../util/apiEndpoints";
 import axiosConfig from "../util/axiosConfig";
 import IncomeList from "../components/IncomeList";
+import Model from "../components/Model";
+import { icons, Plus } from "lucide-react";
+import AddIncomeForm from "../components/AddIncomeForm";
+import toast from "react-hot-toast";
 
 const Income = () => {
   const [incomeData, setIncomeData] = useState([]);
@@ -33,28 +37,110 @@ const Income = () => {
     }
   };
 
+  const fetchCategoryData = async () => {
+    try {
+      const response = await axiosConfig.get(
+        `${API_ENDPOINTS.CATEGORY}/Income`
+      );
+      if (response.status === 200) {
+        setCategoryData(response.data);
+        console.log("Category Data: ", response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching category data:", error);
+      toast.error("Failed to fetch category data");
+    }
+  };
+
   useEffect(() => {
     fetchIncomeData();
+    fetchCategoryData();
   }, []);
 
   useUser();
+
+  const handleAddIncome = async (incomeData) => {
+    setLoading(true);
+    const { name, amount, date, categoryId, icon } = incomeData;
+    if (!name || !amount || !date || !categoryId) {
+      toast.error("Please fill all fields");
+      setLoading(false);
+      return;
+    }
+    if (Number(amount) <= 0) {
+      toast.error("Amount should be greater than 0");
+      setLoading(false);
+      return;
+    }
+    const today = new Date().toISOString().split("T")[0];
+    if (today < date) {
+      toast.error("Date cannot be in the future");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      console.log("Adding income with data:", {
+        name,
+        amount,
+        date,
+        categoryId,
+        icon,
+      });
+      const response = await axiosConfig.post(`${API_ENDPOINTS.INCOME}/add`, {
+        name,
+        amount,
+        date,
+        categoryId,
+        icon,
+      });
+      if (response.status === 201) {
+        toast.success("Income added successfully");
+      }
+    } catch (error) {
+      console.error("Error adding income:", error);
+      toast.error("Failed to add income");
+    } finally {
+      setLoading(false);
+      setOpenIncomeModal(false);
+      fetchIncomeData();
+    }
+  };
+
   return (
     <Dashboard activeMenu="Income">
-        <div className="my-5 mx-auto">
-            <div className="grid grid-cols-1 gap-6">
-                <div>
-                    <h2 className="text-2xl font-bold">Income</h2>
-                    <p className="text-gray-600">Manage your income transactions here.</p>
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div>
+            <button
+              onClick={() => setOpenIncomeModal(true)}
+              className="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-white bg-emerald-600 rounded-md hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              <Plus size={16} />
+              Add Income
+            </button>
+          </div>
+          <IncomeList
+            transactions={incomeData}
+            onDelete={(id) => {
+              setOpenDeleteModal({ show: true, data: id });
+            }}
+          />
 
-                </div>
-                <IncomeList transactions={incomeData} 
-                onDelete={(id) => {
-                    console.log("Delete ID: ", id);
-                }}/>
-            </div>
+          <Model
+            isOpen={openIncomeModal}
+            onClose={() => setOpenIncomeModal(false)}
+            title="Add Income Transaction"
+          >
+            <AddIncomeForm
+              onAddIncome={(incomeData) => handleAddIncome(incomeData)}
+              categories={categoryData}
+            />
+          </Model>
         </div>
-  </Dashboard>
-  )
+      </div>
+    </Dashboard>
+  );
 };
 
 export default Income;
