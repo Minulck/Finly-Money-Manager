e4aef662ecf2d413a4e391da5a41e9608e458170
minulck <minulck@gmail.com>
2025-08-06 16:01:14 +0530

    Add manifest.json and service worker for Finly Money Manager
    - Created manifest.json to define app metadata, including name, description, icons, and display settings.
    - Implemented service worker (service-worker.js) to cache images using a Network First strategy for improved performance.

---
+++ finly-webapp/service-worler.js
@@ -0,0 +1,6 @@
+import ("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === 'image',
+    new workbox.strategies.NetworkFirst()
+);
