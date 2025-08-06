98bd9498c0e8b50fec35bd75b03541b6a2ed83e2
minulck <minulck@gmail.com>
2025-08-06 16:16:50 +0530

    fix: Improve code formatting and consistency in MenuBar component

--- finly-webapp/src/components/MenuBar.jsx
+++ finly-webapp/src/components/MenuBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState, useRef, useContext } from "react";
+import React, { useEffect, useState, useRef, useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { Link } from "react-router-dom";
 import { LogOut, Menu, Settings, User, X } from "lucide-react";
@@ -6,18 +6,18 @@ import { assets } from "../assets/assets";
 import SideBar from "./SideBar";
 import { useNavigate } from "react-router-dom";
 
-const MenuBar = ({activeMenu}) => {
+const MenuBar = ({ activeMenu }) => {
   const [openSideMenu, setOpenSideMenu] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
   const dropDownRef = useRef(null);
   const { user, clearUser } = useContext(AppContext);
 
-  const handleDropdown=()=>{
+  const handleDropdown = () => {
     setShowDropdown(!showDropdown);
-  }
+  };
 
   const navigator = useNavigate();
-  
+
   const handleLogout = () => {
     localStorage.clear();
     setShowDropdown(false);
@@ -25,19 +25,19 @@ const MenuBar = ({activeMenu}) => {
     window.location.href = "/login";
   };
 
- useEffect(() => {
-  function handleClickOutside(event) {
-    if (dropDownRef.current && !dropDownRef.current.contains(event.target)) {
-      setShowDropdown(false);
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropDownRef.current && !dropDownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
     }
-  }
-  if (showDropdown) {
-    document.addEventListener("mousedown", handleClickOutside);
-  }
-  return () => {
-    document.removeEventListener("mousedown", handleClickOutside);
-  };
-}, [showDropdown]);
+    if (showDropdown) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showDropdown]);
 
   return (
     <div className="flex items-center justify-between gap-5 bg-white border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-4 sm:px-7 sticky top-0 z-30">
@@ -59,54 +59,57 @@ const MenuBar = ({activeMenu}) => {
       </div>
 
       {/* right side - avatar photo  */}
-     <div className="relative" ref={dropDownRef}>
-        <button 
-        onClick={handleDropdown}
-        className="flex  item-center justify-center px-5 py-2 bg-gray-100 hover:bg-gray-200 rounded-full transition-colors duration-200 focus:outline-none text-black focus:ring-2 focus:ring-emerald-800 focus:ring-offset-2">
-            <User className="m-auto text-emerald-500 mr-4  rounded-" size={30} />
-            <p className="flex items-center justify-center">{user.fullName}</p>
+      <div className="relative" ref={dropDownRef}>
+        <button
+          onClick={handleDropdown}
+          className="flex  item-center justify-center px-5 py-2 bg-gray-100 hover:bg-gray-200 rounded-full transition-colors duration-200 focus:outline-none text-black focus:ring-2 focus:ring-emerald-800 focus:ring-offset-2"
+        >
+          <User className="m-auto text-emerald-500 mr-4  rounded-" size={30} />
+          <p className="flex items-center justify-center">
+            {(user.fullName || "").split(" ")[0]}
+          </p>
         </button>
         {showDropdown && (
           <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded shadow-xl z-50">
             <div className="px-4 py-3 border-gray-100">
-                <div className="flex item-center gap-3">
-                    <div className="flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full">
-                        <User className="w-4 h-4 text-emerald-600"/>
-                    </div>
-                    <div className="flex-1 min-w-0">
-                        <p className="text-sm font-medium text-gray-800 truncate">
-                            {user.fullName || user.email}
-                        </p>
-                        <p className="text-xs text-gray-500 truncate">
-                            {user.email}
-                        </p>
-                    </div>
+              <div className="flex item-center gap-3">
+                <div className="flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full">
+                  <User className="w-4 h-4 text-emerald-600" />
                 </div>
-            </div>
-             <div className="py-1">
-                <button 
-                onClick={()=>navigator("/settings")}
-                className="flex item-center gap-3 w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-150 cursor-pointer">
-                    <Settings className="w-4 h-4 text-gray-500"/>  
-                    <span>Settings</span>
-                </button>
+                <div className="flex-1 min-w-0">
+                  <p className="text-sm font-medium text-gray-800 truncate">
+                    {user.fullName}
+                  </p>
+                  <p className="text-xs text-gray-500 truncate">{user.email}</p>
+                </div>
+              </div>
             </div>
             <div className="py-1">
-                <button 
+              <button
+                onClick={() => navigator("/settings")}
+                className="flex item-center gap-3 w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-150 cursor-pointer"
+              >
+                <Settings className="w-4 h-4 text-gray-500" />
+                <span>Settings</span>
+              </button>
+            </div>
+            <div className="py-1">
+              <button
                 onClick={handleLogout}
-                className="flex item-center gap-3 w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-150 cursor-pointer">
-                    <LogOut className="w-4 h-4 text-gray-500"/> 
-                    <span>Logout</span>
-                </button>
+                className="flex item-center gap-3 w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition-colors duration-150 cursor-pointer"
+              >
+                <LogOut className="w-4 h-4 text-gray-500" />
+                <span>Logout</span>
+              </button>
             </div>
           </div>
         )}
-     </div>
+      </div>
       {/* mobile view  */}
-      {openSideMenu && ( 
+      {openSideMenu && (
         <div className="fixed left-0 right-0 bg-white border-b border-gray-200 lg:hidden z-20 top-[73px]">
-            <SideBar activeMenu={activeMenu} />
-        </div> 
+          <SideBar activeMenu={activeMenu} />
+        </div>
       )}
     </div>
   );
